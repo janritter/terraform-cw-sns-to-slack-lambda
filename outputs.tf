8ee9f2cb3092bb42ca2a62f771ae94135554106a
Jonas Reineke <jonas.reineke@trustedshops.de>
2021-04-13 12:00:59 +0200

    feat: add more lambda and iam role outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,18 @@
 output "lambda_arn" {
   value = aws_lambda_function.lambda.arn
 }
+
+output "lambda_function_name" {
+  value      = aws_lambda_function.lambda.function_name
+  depends_on = "Name of the function"
+}
+
+output "iam_role_arn" {
+  value       = aws_iam_role.lambda_exec_role.arn
+  description = "ARN of the IAM role of the lambda function"
+}
+
+output "iam_role_name" {
+  value       = aws_iam_role.lambda_exec_role.name
+  description = "Name of the IAM role of the lambda function"
+}
